06d22ef34c7848bcc9504fb1f2f2c2d6656d7c6b
Hesam Mousavi <hesammoousavi@gmail.com>
2024-09-09 18:09:09 +0330

    add vite and use it

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,10 @@
 
     <h3>add sidebar section</h3>
 
-
 @endsection
 
 @section('content')
     <h2>The Content</h2>
+{{--    <img src="/image/qmLca1lqtoFe1kwJmBGe5lJ9ET17agVL2D8nQGwb.jpg" width="400" alt="">--}}
     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis doloremque earum enim facere hic impedit, ipsa necessitatibus, nihil nulla officiis quasi quis sunt vero voluptates! Debitis obcaecati placeat quia?</p>
 @endsection
